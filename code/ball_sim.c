57d935dc199de194114cf4e4494617e3dca81434
TanatornBig <tanatorn.nateesanprasert@gmail.com>
2019-04-25 20:00:36 +0700

    check ball valid

--- code/ball_sim.c
+++ code/ball_sim.c
@@ -46,26 +46,28 @@ int main(int argc, char **argv)
     int processBallBoundNumber;
 
     /* Ball's Information */
-    double *ballMass;           /* Array data of all ball's mass */
-    double *ballElectricCharge; /* Array data of all ball's charge */
-    double *ballXPosition;      /* Array data of all ball's x position */
-    double *ballYPosition;      /* Array data of all ball's y position */
-    double *ballXVelocity;      /* Array data of all ball's x velocity */
-    double *ballYVelocity;      /* Array data of all ball's y velocity */
-    double *newBallXPosition;   /* Array data of all ball's x position */
-    double *newBallYPosition;   /* Array data of all ball's y position */
-    double *newBallXVelocity;   /* Array data of all ball's x velocity */
-    double *newBallYVelocity;   /* Array data of all ball's y velocity */
+    double *ballMass;             /* Array data of all ball's mass */
+    double *ballElectricCharge;   /* Array data of all ball's charge */
+    double *ballXPosition;        /* Array data of all ball's x position */
+    double *ballYPosition;        /* Array data of all ball's y position */
+    double *ballXVelocity;        /* Array data of all ball's x velocity */
+    double *ballYVelocity;        /* Array data of all ball's y velocity */
+    int *ballValid                /* Array data of all ball's status if ball has x or y less than 100000 */
+        double *newBallXPosition; /* Array data of all ball's x new position */
+    double *newBallYPosition;     /* Array data of all ball's y new position */
+    double *newBallXVelocity;     /* Array data of all ball's x new velocity */
+    double *newBallYVelocity;     /* Array data of all ball's y new velocity */
+    int *newBallValid             /* Array data of all ball's status if ball is still valid in the next step */
 
-    /*Force and Accel Information*/
-    double force;
+        /*Force and Accel Information*/
+        double force;
     double forceSize;
     double deltaXVector;
     double deltaYVector;
     double cumulativeXForce = 0;
     double cumulativeYForce = 0;
-    double accerolateX;
-    double accerolateY;
+    double accelerateX;
+    double accelerateY;
 
     /* I J K */
     int i;
@@ -93,7 +95,7 @@ int main(int argc, char **argv)
         fscanf(fp, "%lf\n", &timeStep);
     }
 
-    /* Broaccast data */
+    /* Broadcast data */
     MPI_Bcast(&ballNumber, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&simNumber, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&timeStep, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -105,12 +107,14 @@ int main(int argc, char **argv)
     ballYPosition = (double *)malloc(sizeof(double) * ballNumber);
     ballXVelocity = (double *)malloc(sizeof(double) * ballNumber);
     ballYVelocity = (double *)malloc(sizeof(double) * ballNumber);
+    ballValid = (int *)malloc(sizeof(int) * ballNumber);
 
     /* Allocate array for next step Nth ball */
     newBallXPosition = (double *)malloc(sizeof(double) * ballNumber);
     newBallYPosition = (double *)malloc(sizeof(double) * ballNumber);
     newBallXVelocity = (double *)malloc(sizeof(double) * ballNumber);
     newBallYVelocity = (double *)malloc(sizeof(double) * ballNumber);
+    newBallValid = (int *)malloc(sizeof(int) * ballNumber);
 
     if (id == 0)
     {
@@ -118,6 +122,8 @@ int main(int argc, char **argv)
         for (i = 0; i < ballNumber; i++)
         {
             fscanf(fp, "%lf %lf %lf %lf %lf %lf\n", &ballMass[i], &ballElectricCharge[i], &ballXPosition[i], &ballYPosition[i], &ballXVelocity[i], &ballYVelocity[i]);
+            /* 1 = still valid and 0 = not valid anymore */
+            ballValid[i] = 1;
         }
     }
 
@@ -128,6 +134,7 @@ int main(int argc, char **argv)
     MPI_Bcast(&ballYPosition, ballNumber, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&ballXVelocity, ballNumber, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&ballYVelocity, ballNumber, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&ballValid, ballNumber, MPI_INT, 0, MPI_COMM_WORLD);
 
     /* Calculate each process ball number */
     processBallNumber = ballNumber / numProc;
@@ -157,45 +164,49 @@ int main(int argc, char **argv)
     /* Last ball in each rank */
     processBallBoundNumber = processBallStartNumber + processBallNumber;
 
-    // /* Looping K time (Simulation number retreive from input file */
-    // for (k = 0; k < simNumber; k++)
-    // {
-
-    // }
-
-    /* Looping all ball in each rank to calculate accels and velocity */
-    for (i = processBallStartNumber; i < processBallBoundNumber; i++)
+    /* Looping K time (Simulation number retreive from input file */
+    for (k = 0; k < simNumber; k++)
     {
-        /* Looping all ball */
-        for (j = 0; j < ballNumber; j++)
+        /* Looping all ball in each rank to calculate accels and velocity */
+        for (i = processBallStartNumber; i < processBallBoundNumber; i++, cumulativeXForce = 0, cumulativeYForce = 0)
         {
-            /* Second ball is not equal to the first one */
-            if (j != i)
+            if (ballValid[i])
             {
-                /* Calculate Electric Force on the ball */
-                /* F = (q1 x q2) / ((xi - xj)^2 + (yi - yj)^2) */
-                force = (ballElectricCharge[i] * ballElectricCharge[j]) / (((ballXPosition[i] - ballXPosition[j]) * (ballXPosition[i] - ballXPosition[j])) + ((ballYPosition[i] - ballYPosition[j]) * (ballYPosition[i] - ballYPosition[j])));
+                /* Looping all ball */
+                for (j = 0; j < ballNumber; j++)
+                {
+                    /* Second ball is not equal to the first one */
+                    if (ballValid[j] && j != i)
+                    {
+                        /* Calculate Electric Force on the ball */
+                        /* F = (q1 x q2) / ((xi - xj)^2 + (yi - yj)^2) */
+                        force = (ballElectricCharge[i] * ballElectricCharge[j]) / (((ballXPosition[i] - ballXPosition[j]) * (ballXPosition[i] - ballXPosition[j])) + ((ballYPosition[i] - ballYPosition[j]) * (ballYPosition[i] - ballYPosition[j])));
 
-                /* Find force vector in x axis */
-                deltaXVector = ballXPosition[j] - ballXPosition[i];
-                /* Find force vector in y axis */
-                deltaYVector = ballYPosition[j] - ballYPosition[i];
-                /* size of force = sqrt((xj-xi)^2 + (yj-yi)^2)) */
-                forceSize = sqrt(deltaXVector * deltaXVector + deltaYVector * deltaYVector);
-                /* Calculate cumulativeXForce vector and cumulativeYForce vector */
-                cumulativeXForce += force * deltaXVector / forceSize;
-                cumulativeYForce += force * deltaYVector / forceSize;
+                        /* Find force vector in x axis */
+                        deltaXVector = ballXPosition[j] - ballXPosition[i];
+                        /* Find force vector in y axis */
+                        deltaYVector = ballYPosition[j] - ballYPosition[i];
+                        /* size of force = sqrt((xj-xi)^2 + (yj-yi)^2)) */
+                        forceSize = sqrt(deltaXVector * deltaXVector + deltaYVector * deltaYVector);
+                        /* Calculate cumulativeXForce vector and cumulativeYForce vector */
+                        cumulativeXForce += force * deltaXVector / forceSize;
+                        cumulativeYForce += force * deltaYVector / forceSize;
+                    }
+                }
+                /* Summarise and calculate ball's accelorate */
+                accelerateX = cumulativeXForce / ballMass[i];
+                accelerateY = cumulativeYForce / ballMass[i];
+
+                /* Collect next ball's position and velocity */
+                newBallXPosition[i] = ballXPosition[i] + (timeStep * ballXVelocity[i]);
+                newBallYPosition[i] = ballYPosition[i] + (timeStep * ballYVelocity[i]);
+                newBallXVelocity[i] = ballXVelocity[i] + (timeStep * accelerateX);
+                newBallYVelocity[i] = ballYVelocity[i] + (timeStep * accelerateY);
+                /* If new X or Y position is more than 100000 then this ball is not valid anymore */
+                newBallValid[i] = (newBallXPosition[i] > 1000000 || newBallYPosition[i] > 1000000) : 0 ? 1;
             }
         }
 
-        /* Summarise and calculate ball's accelorate */
-        accerolateX = cumulativeXForce / ballMass[i];
-        accerolateY = cumulativeYForce / ballMass[i];
-
-        /* Collect next ball's position and velocity */
-        newBallXPosition[i] = ballXPosition[i] + (timeStep * ballXVelocity[i]);
-        newBallYPosition[i] = ballYPosition[i] + (timeStep * ballYVelocity[i]);
-        newBallXVelocity[i] = ballXVelocity[i] + (timeStep * accerolateX);
-        newBallYVelocity[i] = ballYVelocity[i] + (timeStep * accerolateY);
+        /* Every ranks isend data to adjacent ranks */
     }
 }
